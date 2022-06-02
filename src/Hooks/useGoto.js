514ecdec0a65f723fd41e8275161459a91579617
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2022-06-02 08:06:12 -0300

    hook usego

--- src/Hooks/useGoto.js
+++ src/Hooks/useGoto.js
@@ -1,8 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import {env} from "../App/Config/config";
 
-export default function useGoto(){
+export default function useGoto(state=null){
     const navigate = useNavigate()
-    const to = u => navigate(env.BASEURL+"/"+u);
+    const to = u => navigate(env.BASEURL+"/"+u,{state:state});
     return {to};
 }
