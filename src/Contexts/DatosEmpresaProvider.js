ad72846f4f0ad1522fb842886bee6efbafaa8bf1
derlisrd <derlisruizdiaz@hotmail.com>
2022-06-16 16:44:54 -0300

    data mondea

--- src/Contexts/DatosEmpresaProvider.js
+++ src/Contexts/DatosEmpresaProvider.js
@@ -9,25 +9,36 @@ const DatosEmpresaProvider = ({children}) => {
     const storageMoneda = JSON.parse(localStorage.getItem("dataMonedas"));
     const mon = storageMoneda ? storageMoneda.filter(e=> e.activo_moneda==="1" ||e.activo_moneda===1) : {} 
 
+    
 
     const [MONEDA_PRINCIPAL,SET_MONEDA_PRINCIPAL] = useState(mon[0]);
     const [EMPRESA,SET_EMPRESA] = useState(storageEmpresa ? storageEmpresa : {})
     
+  
+    //console.log("STATE MONEDA", MONEDA_PRINCIPAL)
     const {userData} = useLogin()
     const {login} = userData
 
+    
     const getDatos = useCallback(async()=>{
-        
-      if(storageEmpresa===null || storageMoneda===null){
+        let storeMoneda = localStorage.getItem("dataMonedas"), storeEmpresa = localStorage.getItem("dataEmpresa");
+      if(storeEmpresa===null || storeMoneda===null){
           if(login){
           let promise = await Promise.all([APICALLER.get({table:'monedas',where:`activo_moneda,=,1`}),APICALLER.get({table:'empresas'})])
-          let res = promise[0] 
-          res.response==="ok" ? SET_MONEDA_PRINCIPAL(res.results[0]) : console.log(res)
-          let emp = promise[1]
-          emp.response==="ok" ? SET_EMPRESA(emp.results[0]) : console.log(emp);
+          let res = promise[0],emp = promise[1];
+          if(res.response==="ok" && emp.response==="ok"){
+            SET_EMPRESA(emp.results[0])
+            localStorage.setItem("dataEmpresa",JSON.stringify(emp.results[0]))
+            SET_MONEDA_PRINCIPAL(res.results[0])
+            localStorage.setItem("dataMonedas",JSON.stringify(res.results))
+          }
         }
+      }else{ 
+        let moneda = JSON.parse(storeMoneda).filter(e=> e.activo_moneda==="1" ||e.activo_moneda===1)
+        SET_MONEDA_PRINCIPAL(moneda[0])
+        SET_EMPRESA(JSON.parse(storeEmpresa))
       }
-    },[login,storageEmpresa,storageMoneda])
+    },[login])
 
     useEffect(() => {
         const ca = new AbortController();
