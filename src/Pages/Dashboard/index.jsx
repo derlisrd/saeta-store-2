92ba2dc128c1d1fc84ccdeaf1a940d69e15be823
derlisrd <derlisruizdiaz@hotmail.com>
2022-06-27 20:45:48 -0400

    derlis

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,22 +1,36 @@
-import { Button, Card, CardContent, Grid, Typography } from "@mui/material";
+import { Button, Card, CardContent, CircularProgress, Grid, Typography } from "@mui/material";
 import { useCallback } from "react";
-import { useEffect,/* useState */ } from "react";
+import { useEffect,useState } from "react";
 import { useLang } from "../../Contexts/LangProvider";
 import { useLogin } from "../../Contexts/LoginProvider";
 const DashBoard = () => {
   const {lang} = useLang();
   const {userData} = useLogin();
-  //const [cotizacion,setCotizacion] = useState([]);
+  const [loading,setLoading] = useState({
+    general:true
+  })
+  const [cotizacion,setCotizacion] = useState({
+    bcp:{
+      compra: 0,
+      venta: 0
+    },
+    cambioschaco:{
+      compra: 0,
+      venta: 0
+    },
+    fecha:""
+  });
   let ndate = new Date();
   let hours = ndate.getHours();
   let message = hours < 12 ? lang.buen_dia : hours < 18 ? lang.buenas_tardes : lang.buenas_noches;
 
   const getCotizacion = useCallback( async()=>{
+    setLoading({general:true});
     let res = await fetch ('https://dolar.melizeche.com/api/1.0/');
     let data = await res.json();
-    console.log(data)
+    setCotizacion(data?.dolarpy);
+    setLoading({general:false});
   },[])
-
 
   useEffect(() => {
     const ca = new AbortController();
@@ -43,18 +57,29 @@ const DashBoard = () => {
           </CardContent>
         </Card>
      </Grid>
-     <Grid item xs={12} sm={12} md={12} lg={4} xl={3} >
-        <Card >
+     <Grid item xs={12} sm={6} md={6} lg={4} xl={3} >
+     <Card >
           <CardContent>
-            <Typography variant="button">Cotizacion del dia</Typography>
+            {
+              loading.general ?
+            <CircularProgress />
+              :
+            (<><Typography variant="h6">COTIZACION DEL DIA DOLAR AMERICANO</Typography>
             <br />
-            <Typography variant="overline">Bienvenido nuevamente</Typography>
+            <Typography variant="button">
+              Compra: {cotizacion?.bcp.compra}
+            </Typography>
             <br />
-            <Button variant="contained">{lang.iniciar}</Button>
+            <Typography variant="button">
+              Venta: {cotizacion?.bcp.venta}
+            </Typography>
+            <br />
+            </>)
+          }
           </CardContent>
         </Card>
+        
      </Grid>
-    
 
     </Grid>
   )
