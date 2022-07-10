18d5bcfe9dd438a9ae8a84dddacffd29881e428b
derlisrd <derlisruizdiaz@hotmail.com>
2022-07-10 01:30:03 -0400

    relatorio productos

--- src/Pages/Informes/Productos/InformesProductosProvider.js
+++ src/Pages/Informes/Productos/InformesProductosProvider.js
@@ -1,30 +1,61 @@
-import React, { createContext, useContext, useState,useCallback,useEffect } from 'react';
+import  { createContext, useContext, useState,useCallback,useEffect } from 'react';
 import { useLang } from '../../../Contexts/LangProvider';
-
+import { APICALLER } from '../../../Services/api';
+import { funciones } from '../../../Functions';
 
 const Contexto = createContext();
 
 function InformesProductosProvider({children}) {
     
     const {lang} = useLang()
+    
 
     const initialLoadings = {
-        general:true,
-        lista:true
+        general:false,
+        listas:true
     }
 
     const initialListas = {
-        productos_vendidos: []
+        lista: []
     }
+
+    const initialFechas = {
+        desde: funciones.fechaActualYMD(),
+        hasta:funciones.fechaActualYMD()
+    }
+
+    const [fechas,setFechas] = useState(initialFechas)
 
     const [loadings,setLoadings] = useState(initialLoadings)
     const [listas,setListas] = useState(initialListas)
     
-    const values = {
-        loadings,lang,listas
-    }
     
-    return ( <Contexto.Provider value={values}>
+    const getData = useCallback(async()=>{
+        setLoadings({listas:true})
+        let res = await APICALLER.get({table:"productos_vendidos",include:"productos",on:"id_producto,id_producto_vendido",
+        fields:"id_producto_vendido,nombre_producto,fecha_vendido,precio_vendido,costo_producto_vendido,cantidad_vendido",
+        where:`fecha_vendido,between,'${fechas.desde} 00:00:00',and,'${fechas.hasta} 23:59:59'`});
+        res.response==="ok" ? setListas({lista:res.results}) : console.log(res);
+        setLoadings({listas:false})
+    },[fechas])
+    
+    useEffect(() => {
+        const ca = new AbortController();
+        let isActive = true;
+        if (isActive) {
+          getData();
+        }
+        return () => {
+          isActive = false;
+          ca.abort();
+        };
+      }, [getData]);
+    
+    const values = {
+        loadings,lang,listas,fechas,setFechas,funciones
+    }
+    return (
+    <Contexto.Provider value={values}>
         {children}
     </Contexto.Provider> );
 }
@@ -32,8 +63,8 @@ function InformesProductosProvider({children}) {
 
 export function useInformesProductos(){
 
-    const {loadings,lang,listas} = useContext(Contexto)
-    return {loadings,lang,listas}
+    const {loadings,lang,listas,fechas,setFechas,funciones} = useContext(Contexto)
+    return {loadings,lang,listas,fechas,setFechas,funciones}
 }
 
 export default InformesProductosProvider;
--- src/Pages/Informes/Productos/Lista.jsx
+++ src/Pages/Informes/Productos/Lista.jsx
@@ -1,30 +1,107 @@
+import { Alert, Button, Fab, Grid, Icon, Stack, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 import Tablas from "../../../Components/UI/Tablas";
 import { useInformesProductos } from "./InformesProductosProvider";
 
+
+
 function Lista() {
     
-    const {loadings,listas,lang} = useInformesProductos()
-    const columns = [];
+    const {loadings,listas,lang,fechas,setFechas,funciones} = useInformesProductos()
     
+
+    const [fecha,setFecha] = useState(fechas)
+    //id_producto_vendido,nombre_producto,fecha_vendido,precio_vendido,costo_producto_vendido,cantidad_vendido
+    const columns = [
+        {
+            field: "id_producto_vendido",
+            title: "#",
+          },
+          {
+            field: "cantidad_vendido",
+            title: "Cantidad",
+            isNumber:true
+          },
+          {
+            field: "nombre_producto",
+            title: "Producto",
+          },
+          {
+            field: "costo_producto_vendido",
+            title: "Costo",
+            isNumber:true
+          },
+          {
+            field: "precio_vendido",
+            title: "Precio",
+            isNumber:true
+          },
+          {
+            field: "fecha_vendido",
+            title: "Fecha",
+          },
+    ];
     
+    const change = e=>{
+        setFecha({...fecha,[e.target.name]:e.target.value})
+    }
+    const filtrar = (e)=>{
+        e.preventDefault();
+        setFechas(fecha)
+    }
     
     const Acciones = ({rowProps})=>(
-        <></>
+        <Stack direction="row" spacing={2}>
+            <Fab size="small"><Icon>settings</Icon></Fab>
+        </Stack>
     )
 
     const filters = (
-        <>
-        </>
+
+        <Grid container spacing={2} >
+            <Grid item xs={12} sm={6} md={2}>
+                <TextField 
+                    type="date" fullWidth
+                    onChange={change}
+                    value={fecha.desde}
+                    label={lang.desde}
+                    name="desde"
+                />
+            </Grid>
+            <Grid item xs={12} sm={6} md={2}>
+                <TextField 
+                    type="date" fullWidth
+                    onChange={change}
+                    value={fecha.hasta}
+                    label={lang.hasta}
+                    name="hasta"
+                />
+            </Grid>
+            <Grid item xs={12} sm={6} md={2}>
+                <Button onClick={filtrar} size="large" variant="contained" >Filtrar</Button>
+            </Grid>
+            <Grid item xs={12}>
+                <Grid container>
+                    <Grid item xs={12} sm={6} md={4}>
+                    <Alert severity="success" variant="outlined" icon={false}>
+                        <Typography variant="h6">
+                            {lang.lucro}: {funciones.numberSeparator(10000)}{" "}
+                        </Typography>
+                    </Alert>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 
 
     return (<Tablas
-        title={lang.cotizaciones_monedas}
-        subtitle={``}
-        icon={{ name:"people" }}
+        title={lang.informes_productos_vendidos}
+        subtitle={lang.sub_informes_productos_vendidos}
+        icon={{ name:"summarize" }}
         columns={columns}
         loading={loadings.listas}
-        datas={listas.productos_vendidos}
+        datas={listas.lista}
         Accions={Acciones}
         inputs={filters}
         lang={lang}
