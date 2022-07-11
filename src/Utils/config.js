767a141aef7b76b98b598cc41016ce136720e48d
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2022-07-11 13:53:01 -0300

    updates novedades

--- src/Utils/config.js
+++ src/Utils/config.js
@@ -4,7 +4,7 @@ export const BASEURL =  process.env.REACT_APP_BASEURL; //'/vikki'; // aqui debe 
 export const ICONAPP = process.env.REACT_APP_ICON; //export const ICONAPP = 'content_cut'; // DEBE ESTAR INSTALADO GOOGLE FONTS ICONS https://fonts.google.com/icons?selected=Material+Icons
 export const APIURL = process.env.REACT_APP_API_URL; // ejemplo: http://dominio.com/api/
 export const APPNAME = process.env.REACT_APP_NAME;
-export const XAPITOKEN = process.env.REACT_APP_API_KEY_TOKEN;
+export const XAPITOKEN = process.env.REACT_APP_X_API_TOKEN;
 export const SECRETO =  process.env.REACT_APP_SECRETO;
 
 
@@ -13,6 +13,6 @@ export const env = {
     ICONAPP : process.env.REACT_APP_ICON, //export const ICONAPP = 'content_cut'; // DEBE ESTAR INSTALADO GOOGLE FONTS ICONS https://fonts.google.com/icons?selected=Material+Icons
     APIURL : process.env.REACT_APP_API_URL, // ejemplo: http://dominio.com/api/
     APPNAME : process.env.REACT_APP_NAME,
-    XAPITOKEN : process.env.REACT_APP_API_KEY_TOKEN,
+    XAPITOKEN : process.env.REACT_APP_X_API_TOKEN,
     SECRETO :  process.env.REACT_APP_SECRETO
 }
