65617c0fd7aded1c04c80e6fbba900d975015574
derlisrd <derlisruizdiaz@hotmail.com>
2022-08-07 14:15:10 -0400

    copia

--- src/Pages/Backup/index.tsx
+++ src/Pages/Backup/index.tsx
@@ -1,10 +1,20 @@
+import { Box, Grid, Link, Typography } from '@mui/material'
 import React from 'react'
 
 const Backup = () => {
   return (
-    <>
-     <h1>Hacer copia de seguridad</h1> 
-    </>
+    <Box>
+     <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant='button'>Hacer una copia de seguridad diario es importante para mantener los datos seguros</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          
+          <Link href="#">Descargar copia de seguridad</Link>
+            
+        </Grid>
+     </Grid>
+    </Box>
   )
 }
 
