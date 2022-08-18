02628d6d3351868f314562e97da856ef6ad2a152
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2022-08-17 21:37:41 -0400

    url

--- src/Pages/Backup/index.tsx
+++ src/Pages/Backup/index.tsx
@@ -1,10 +1,11 @@
 import { Box, Grid, Link, Typography } from '@mui/material'
 import React from 'react'
 import { APIURL } from '../../App/Config/config'
+import { useLogin } from '../../Contexts/LoginProvider'
 
 const Backup = () => {
-
-  const url = APIURL+'backup'
+  const {userData} = useLogin()
+  const url = APIURL+'backup?'+userData.token_user
 
   return (
     <Box>
