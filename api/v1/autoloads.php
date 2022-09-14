4841f9b77fa30b5a5fa6514bd989477054f05b24
derlisrd <derlisruizdiaz@hotmail.com>
2022-09-13 22:37:49 -0400

    derlis

---
+++ api/v1/autoloads.php
@@ -0,0 +1,23 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept,X-Api-Token,Authorization");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+require "./vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+require "./Config/helper.php";
+require "./Globals/Globals.php";
+require "./Config/DataBaseConnect.php";
+require "./Controllers/Controllers.php";
+require "./Controllers/GetController.php";
+require "./Controllers/ExportController.php";
+require "./Controllers/PutController.php";
+require "./Controllers/PostController.php";
+require "./Controllers/DeleteController.php";
+require "./Controllers/AuthController.php";
+require "./Models/Models.php";
+require "./Routes/Routes.php";
+require "./Routes/JsonResponse.php";
