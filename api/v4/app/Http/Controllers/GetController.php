3a5e44532b3c0d11e0c3cccd7758fb4bb0858dcd
derlisrd <derlisruizdiaz@hotmail.com>
2022-09-15 10:44:44 -0300

    derlis!

--- api/v4/app/Http/Controllers/GetController.php
+++ api/v4/app/Http/Controllers/GetController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class GetController extends Controller
 {
-    //
+    public function getTable (Request $request){
+
+        $resposta = [
+            "found"=>0,
+            "total"=>0,
+            "results"=>[],
+            "table"=>$request->table,
+            "response"=>true,
+            "error"=>false,
+            "message"=>""
+        ];
+        $status = 404;
+        if(Schema::hasTable($request->table)){
+            $result = DB::table($request->table)->get();
+            $resposta = [
+                "found"=>0,
+                "total"=>0,
+                "results"=>$result,
+                "table"=>$request->table,
+                "response"=>true,
+                "error"=>false,
+                "message"=>""
+            ];
+            $status = 200;
+        }
+        return response()->json($resposta,$status);
+    }
 }
--- api/v4/routes/web.php
+++ api/v4/routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\GetController;
 use Illuminate\Support\Facades\Route;
 
-/*
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get("/users",function(){
+
 });
- */
-Route::get('/{table}',)->name('GetTables');
+Route::get('/{table}',[GetController::class,'getTable'])->name('GetTables');
