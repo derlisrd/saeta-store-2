c7cc664e33765f74806c510cd6ba333f1b82f347
derlisrd <derlisruizdiaz@hotmail.com>
2022-10-16 23:41:35 -0300

    formas de pago

--- src/Pages/Ventas/FormasPago/DialogEditar.jsx
+++ src/Pages/Ventas/FormasPago/DialogEditar.jsx
@@ -1,12 +1,17 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, LinearProgress, TextField, Zoom } from '@mui/material';
 import React, { useCallback, useEffect, useState } from 'react';
+import swal from 'sweetalert';
 import { useLang } from '../../../Contexts/LangProvider';
+import { useLogin } from '../../../Contexts/LoginProvider';
+import { APICALLER } from '../../../Services/api';
 import { useFormasPago } from './FormasPagoProvider';
 
 function DialogEditar() {
 
     const {lang} = useLang()
-    const {dialogs,setDialogs,formEdit} = useFormasPago()
+    const {userData} = useLogin()
+    const {token_user} = userData;
+    const {dialogs,setDialogs,formEdit,getLista} = useFormasPago()
     const [loading,setLoading] = useState(false)
     const [form,setForm] = useState({
         id_facturas_formas_pago:"",
@@ -19,8 +24,22 @@ function DialogEditar() {
     const close = ()=>{
         setDialogs({...dialogs,editar:false})
     }
-    const enviar = ()=>{
+    const enviar = async ()=>{
         setLoading(true)
+        let f = {...form}
+
+        let res = await APICALLER.update({table:"facturas_formas_pagos",token:token_user,id:f.id_facturas_formas_pago,
+        data:{
+            descripcion_forma_pago: f.descripcion_forma_pago,
+            porcentaje_descuento_pago: f.porcentaje_descuento_pago
+        }})
+        if(res.response==="ok"){
+            swal({text:lang.actualizado_correctamente,icon:"success"})
+        }
+        getLista()
+        setLoading(false)
+        close()
+
     }
 
     const getDatas = useCallback(async()=>{
@@ -48,10 +67,10 @@ function DialogEditar() {
                         {loading && <LinearProgress /> }
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField autoFocus disable={loading} fullWidth onChange={change} name="descripcion_forma_pago" value={form.descripcion_forma_pago} label={lang.descripcion} disabled={loading} />
+                        <TextField autoFocus fullWidth onChange={change} name="descripcion_forma_pago" value={form.descripcion_forma_pago} label={lang.descripcion} disabled={loading} />
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField disable={loading} onChange={change} name="porcentaje_descuento_pago" value={form.porcentaje_descuento_pago} helperText={lang.porcentaje_descuento} disabled={loading} />
+                        <TextField  onChange={change} name="porcentaje_descuento_pago" value={form.porcentaje_descuento_pago} helperText={lang.porcentaje_descuento} disabled={loading} />
                     </Grid>
                 </Grid>
             </DialogContent>
