d76971576f925c7409300fd488904109babe6659
derlisrd <derlisruizdiaz@hotmail.com>
2022-10-18 17:53:47 -0300

    temas

--- src/Contexts/TemaProvider.js
+++ src/Contexts/TemaProvider.js
@@ -164,28 +164,37 @@ const TemaProvider = ({children})=>{
               },
           },
         },
-        }
-        ,
-          MuiListItem:{
+        },
+        MuiListItemButton:{
+          styleOverrides:{
+            root:{
+              "&:hover": {
+                borderRadius:"0 18px 18px 0",
+              },
+            }
+          }
+        },
+        MuiListItem:{
             styleOverrides:{
               root:{
-                borderRadius:"0 10px 10px 0",
+                borderRadius:"0 18px 18px 0",
                 transition:'all 0.02s linear',
                 "&.Mui-selected":{
                   backgroundColor: tema.mode==="light"? colores[tema.colors].primary.light : colores[tema.colors].primary.main,
                   "& span":{
                     /* fontWeight:"bold", */
                     color:tema.mode==="light"? colores[tema.colors].primary.main : colorText,
-                    borderLeft:`3px solid ${colores[tema.colors].primary.main}`,
-                  }
+                  },
+                  borderLeft:`4px solid ${colores[tema.colors].primary.main}`,
                 },
                 "&:hover": {
                   backgroundColor:colores[tema.colors].primary.light,
                   "& span":{
                     color:tema.mode==="light"? colores[tema.colors].primary.main : colorText,
                     /* fontWeight:"bold", */
-                  }
+                  },
                 },
+                
             },
           },
         }, 
