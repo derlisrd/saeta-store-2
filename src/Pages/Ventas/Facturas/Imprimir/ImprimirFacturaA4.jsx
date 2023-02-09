135c83cb573e3463db44ce38d3e0fafd5414a15d
derlisrd <derlisruizdiaz@hotmail.com>
2023-02-09 10:19:16 -0300

    arreglando facturas

--- src/Pages/Ventas/Facturas/Imprimir/ImprimirFacturaA4.jsx
+++ src/Pages/Ventas/Facturas/Imprimir/ImprimirFacturaA4.jsx
@@ -1,4 +1,4 @@
-import { Button, CircularProgress, Dialog, DialogActions, Icon, Stack, Zoom } from '@mui/material'
+import { Button, CircularProgress, Dialog, DialogActions, Icon, Stack, Zoom,Link} from '@mui/material'
 import printJS from 'print-js';
 import styles from './facturaA4.module.css';
 import ButtonCustom from '../../../../Components/MuiCustom/ButtonCustom';
@@ -6,6 +6,7 @@ import { useDatosEmpresa } from '../../../../Contexts/DatosEmpresaProvider';
 import { useLang } from '../../../../Contexts/LangProvider';
 import { useFacturas } from '../FacturasProvider';
 import { funciones } from '../../../../Functions';
+import { APIURL } from '../../../../App/Config/config';
 
 const ImprimirFacturaA4 = () => {
 
@@ -13,7 +14,9 @@ const ImprimirFacturaA4 = () => {
   const {lang} = useLang()
   const {EMPRESA} = useDatosEmpresa()
   const DF = { ...formulario };
-  //const id = DF?.id_factura;  
+  const id = DF?.id_factura;  
+
+  const url_pdf = APIURL+'pdf_factura/'+id;
   const imprimir = () => {
     printJS({ type: "html", printable: "print_factura", 
     style:"#print_factura{font-family:monospace; padding:0; margin:0 auto;} table{border-collapse:collapse; width:210mm; margin:0 auto; border-radius:10px;}" });
@@ -24,7 +27,39 @@ const ImprimirFacturaA4 = () => {
   const enviarEmail = ()=>{
     setDialogs({...dialogs,mail:true})
   }
-  const TOTAL = DF.monto_total_factura
+  //const TOTAL = DF.monto_total_factura
+
+  let TOTAL5 = 0;
+  let TOTAL10 = 0;
+  let TOTALIVA = 0;
+  //let TOTALIVAG = 0;
+  let TOTALIVA5 = 0;
+  let TOTALIVA10 = 0;
+  let EXENTAS = 0;
+  
+  console.log('facturas a 4')
+
+  itemsFactura.forEach(e => {
+    let subtotal = parseFloat(e.precio_producto_factura)* parseFloat(e.cantidad_producto);
+    let iva_porcent = parseInt(e.porcentaje_impuesto);
+    if(iva_porcent === 5){
+      TOTAL5 += subtotal;
+      TOTALIVA5 += (subtotal * iva_porcent) / (100 + iva_porcent);
+    }else
+    if(iva_porcent === 10){
+      TOTAL10 += subtotal;
+      TOTALIVA10 += (subtotal * iva_porcent) / (100 + iva_porcent);
+    }else
+    if(iva_porcent===0){
+      EXENTAS  += subtotal;
+    }
+  });
+  let DESCUENTO = parseFloat(DF.descuento_factura);
+  let MONTO_TOTAL = parseFloat(DF.monto_total_factura) - DESCUENTO
+  
+  TOTALIVA = 0
+  //TOTALIVAG = 0
+
 
 
   return (
@@ -121,23 +156,24 @@ const ImprimirFacturaA4 = () => {
           </table>
           <table width="740" border="1">
             <tbody>
+              <tr><td align='right' colSpan="4">Descuentos: {DESCUENTO}</td></tr>
               <tr>
                 <td>Subtotales</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
+                <td> { EXENTAS}</td>
+                <td>{TOTAL5}</td>
+                <td>{TOTAL10}</td>
               </tr>
               <tr>
                 <td>Total a pagar</td>
                 <td colSpan="2"> 
-                      {funciones.NumeroALetras(TOTAL, "")} {DF.abreviatura_moneda} </td>
-                <td>{TOTAL}</td>
+                      {funciones.NumeroALetras(MONTO_TOTAL, "")} {DF.abreviatura_moneda} </td>
+                <td>{MONTO_TOTAL}</td>
               </tr>
               <tr>
                 <td>Liquidacion de IVA</td>
-                <td>5%: </td>
-                <td>10%: </td>
-                <td>Total IVA: </td>
+                <td>5%: {funciones.redondeo2decimales(TOTALIVA5)}</td>
+                <td>10%: {funciones.redondeo2decimales(TOTALIVA10)} </td>
+                <td>Total IVA:{funciones.redondeo2decimales(TOTALIVA)} </td>
               </tr>
               <tr>
                 <td colSpan="4">
@@ -152,7 +188,10 @@ const ImprimirFacturaA4 = () => {
       )}
       <DialogActions>
         <Button size='large' variant='outlined' onClick={enviarEmail} startIcon={<Icon>alternate_email</Icon>}>Enviar</Button>
-        <Button size='large' variant='outlined' startIcon={<Icon>picture_as_pdf</Icon>}>PDF</Button>
+        <Button size='large' variant='outlined' 
+        component={Link}
+        href={url_pdf}
+        startIcon={<Icon>picture_as_pdf</Icon>}>PDF</Button>
         <ButtonCustom 
           size="large"
           variant="contained"
