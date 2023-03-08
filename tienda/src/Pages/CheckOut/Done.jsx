192c01ee09416a958a32e5a06af2cbfabc92799d
derlisrd <derlisruizdiaz@hotmail.com>
2023-03-08 16:30:20 -0300

    use animation en done

--- tienda/src/Pages/CheckOut/Done.jsx
+++ tienda/src/Pages/CheckOut/Done.jsx
@@ -4,8 +4,9 @@ import github from 'react-useanimations/lib/github';
 
 const Done = () => {
   return (
-    <div className='d-flex mt-5'>
-      <h2>Pedido realizado con exito !  <UseAnimations autoplay animation={github} loop strokeColor="#0066cc" size={56} /> </h2>
+    <div className='d-flex mt-5 justify-content-center'>
+      <h2>Pedido realizado con exito !</h2>
+      <UseAnimations autoplay animation={github} loop strokeColor="#0066cc" size={56} />
     </div>
   )
 }
