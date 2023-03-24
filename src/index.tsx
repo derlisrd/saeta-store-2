9bead3ce3c5a6965a35dd0eab4e0c6fbc61db599
derlisrd <derlisruizdiaz@hotmail.com>
2023-03-24 14:52:18 -0300

    para saeta

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import TemaProvider from "./Contexts/TemaProvider";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import "@fontsource/poppins"
+import 'material-icons/iconfont/filled.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
