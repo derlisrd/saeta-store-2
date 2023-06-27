956fb0a9490d64ba939fadba3711f5adec19994a
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2023-06-27 02:07:12 -0400

    agregando fecha a movimiento

--- src/Functions/index.js
+++ src/Functions/index.js
@@ -37,7 +37,7 @@ export const funciones = {
         date.getFullYear(),
         (date.getMonth() + 1).toString().padStart(2, 0),
         (date.getDate().toString().padStart(2, 0))
-      ].join('-');
+      ].join('-'); 
     },
 
     getDateMDY : (d)=>{
